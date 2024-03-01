cd73989fbaa2c6f07fe220b5d56eed0537c73b4c
Rubén López Expósito <Ruben200410@gmail.com>
2024-03-01 10:16:14 +0100

    practica 2

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -20,13 +20,38 @@ const index = async function (req, res) {
 }
 
 // TODO: Complete the following functions
-
 const create = async function (req, res) {
-
+  try {
+    const newRestaurant = Restaurant.build(req.body)
+    newRestaurant.userId = 1
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{model:Product, as: 'products'}, 'order', 'ASC']],
+    }
+    )
+    res.json(restaurant)
+  }
+  catch (err) {
+    res.status(500).send(err)
+  } 
 }
 
 const update = async function (req, res) {
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -2,6 +2,11 @@ import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
   // TODO: Include routes for restaurant described in the lab session README
-
+  app.route('/restaurants')
+    .get(RestaurantController.index) 
+    .get(RestaurantController.show) 
+    .post(RestaurantController.create)
+    .put(RestaurantController.update)
+    .delete(RestaurantController.delete)
 }
 export default loadFileRoutes
